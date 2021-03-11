07b453bde52eba774fba32141e40fb682d1b156f
Sebbyraft <alessandroseba1995@gmail.com>
2021-03-11 17:04:01 +0100

    New utility function

--- utils.py
+++ utils.py
@@ -25,3 +25,29 @@ def plotDataset(images, labels, classes, columns, rows):
             axes[y,x].axis(False)
 
     plt.show()
+
+def plotCNNhistory(history):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(18, 5))
+
+    x = np.arange(0, len(history.history['accuracy']))
+
+    axes[0].plot(x, history.history['accuracy'], '-*')
+    axes[0].plot(x, history.history['val_accuracy'], '-o')
+    axes[0].legend(['Training Accuracy', 'Validation Accuracy'], fontsize = 14)
+    axes[0].set_title('Training and Validation Accuracy', fontsize=20)
+    axes[0].set_xlabel('Epochs', fontsize = 14)
+    axes[0].set_ylabel('Accuracy', fontsize = 14)
+    axes[0].set_ylim([0,1])
+    axes[0].grid()
+
+    axes[1].plot(x, history.history['loss'], '-*')
+    axes[1].plot(x, history.history['val_loss'], '-o')
+    axes[1].legend(['Training Loss', 'Validation Loss'], fontsize = 14)
+    axes[1].set_title('Training and Validation loss', fontsize = 20)
+    axes[1].set_xlabel('Epochs', fontsize = 14)
+    axes[1].set_ylabel('Loss', fontsize = 14)
+    axes[1].set_ylim([0,None])
+    axes[1].grid()
+
+    fig.tight_layout()
+    plt.show()
